dce44c867144c4daa6bd7c20d069a138d0a0b40e
Evelyn Louise Bauer <evybauer@gmail.com>
2020-02-18 20:04:29 -0800

    Add DialogeTableBookings componnet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
 import Map from "./components/Map";
 import Navbar from "./components/Navbar";
-import AddButton from "./components/AddButton"
+import AddButton from "./components/AddButton";
+import DialogeTableBookings from "./components/DialogeTableBookings";
 
 export default function App() {
+
+    const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const [openDialoge, setOpenDialoge] = React.useState(false);
+  const toggleDialoge = () => {
+    console.log(openDialoge)
+    setOpenDialoge(!openDialoge);
+  };
+
+  const handleClose = () => {
+    setOpenDialoge(false);
+  };
+
   return (
   <div>
-    <Navbar />
+    <Navbar open={open} myBookingsClick={toggleDialoge} />
+    <DialogeTableBookings open={openDialoge} handleClickOpen={handleClickOpen} handleClose={handleClose} />
+  
     <Map />
     <AddButton />
   </div>
