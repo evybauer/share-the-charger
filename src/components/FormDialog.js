323b2e0054180792bfe8fd87754ee60102e960a5
lealinin <lea.linin@gmail.com>
2020-02-21 12:16:12 -0800

    Add FormControl, InputLabel and Select elements from Dropdown, include fullWidth attribute in FormControl

--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -6,7 +6,23 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Dropdown from './Dropdown';
+// import Dropdown from './Dropdown';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+// import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    // margin: theme.spacing(1),
+    minWidth: 340,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
@@ -17,6 +33,13 @@ export default function FormDialog() {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const classes = useStyles();
+  const [minutes, setMinutes] = React.useState('');
+
+  const handleChange = event => {
+    setMinutes(event.target.value);
   };
 
   return (
@@ -37,7 +60,23 @@ export default function FormDialog() {
             type="date"
             fullWidth
           />
-          <Dropdown />
+          <FormControl className={classes.formControl} fullWidth>
+            <InputLabel id="demo-simple-select-label">Minutes</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={minutes}
+              onChange={handleChange}
+            >
+            <MenuItem value={60}>60</MenuItem>
+            <MenuItem value={120}>120</MenuItem>
+            <MenuItem value={240}>240</MenuItem>
+            <MenuItem value={360}>360</MenuItem>
+            <MenuItem value={480}>480</MenuItem>
+            <MenuItem value={600}>600</MenuItem>
+            <MenuItem value={720}>720</MenuItem>
+          </Select>
+        </FormControl>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
