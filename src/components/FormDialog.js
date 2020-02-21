2439946caece5fa67066743871d151d24e90cc8c
lealinin <lea.linin@gmail.com>
2020-02-21 15:25:18 -0800

    Add dropdown inside form dialog

--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -13,8 +13,17 @@ import MenuItem from '@material-ui/core/MenuItem';
 // import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import axios from 'axios';
+// import { Alert, AlertTitle } from '@material-ui/lab';
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+
   formControl: {
     // margin: theme.spacing(1),
     minWidth: 340,
@@ -24,7 +33,15 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function FormDialog() {
+export default function FormDialog(props) {
+  const [form, setForm] = useState({
+    chargerId: "5e48e5635376001a00f75fa3",
+    guestId: "5e48e5635376001a00f75fa3",
+    date: "2020-02-21",
+    minutes: 60,
+    totalPrice: 10
+  })
+
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -36,18 +53,47 @@ export default function FormDialog() {
   };
 
   const classes = useStyles();
+
   const [minutes, setMinutes] = React.useState('');
 
   const handleChange = event => {
     setMinutes(event.target.value);
   };
 
+  const handleInputChange = e => {
+    const { id, value } = e.target;
+    setForm({...form, [id]: value});
+    console.log('THIS IS MY FORM', form);
+
+  }
+
+  const handlePostData = () => {
+    console.log(form);
+    const body = JSON.stringify(form);
+    const url = "http://localhost:8080/reservations";
+
+    axios
+      .post(url, body, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
   return (
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Book
       </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
         <DialogTitle id="form-dialog-title">Book</DialogTitle>
         <DialogContent>
           <DialogContentText>
@@ -59,27 +105,29 @@ export default function FormDialog() {
             id="date"
             type="date"
             fullWidth
+            value={form.date}
           />
           <FormControl className={classes.formControl} fullWidth>
             <InputLabel id="demo-simple-select-label">Minutes</InputLabel>
             <Select
               labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={minutes}
+              id="minutes"
+              value={form.minutes}
               onChange={handleChange}
+              onChange={handleInputChange}
             >
-            <MenuItem value={60}>60</MenuItem>
-            <MenuItem value={120}>120</MenuItem>
-            <MenuItem value={240}>240</MenuItem>
-            <MenuItem value={360}>360</MenuItem>
-            <MenuItem value={480}>480</MenuItem>
-            <MenuItem value={600}>600</MenuItem>
-            <MenuItem value={720}>720</MenuItem>
+              <MenuItem value={60}>60</MenuItem>
+              <MenuItem value={120}>120</MenuItem>
+              <MenuItem value={240}>240</MenuItem>
+              <MenuItem value={360}>360</MenuItem>
+              <MenuItem value={480}>480</MenuItem>
+              <MenuItem value={600}>600</MenuItem>
+              <MenuItem value={720}>720</MenuItem>
           </Select>
         </FormControl>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handlePostData} color="primary">
             Book
           </Button>
           <Button onClick={handleClose} color="primary">
