90c25e8dc2127c05f766d56e5a5970fe0ddf001d
Evelyn Louise Bauer <evybauer@gmail.com>
2020-02-23 21:21:02 -0800

    Add input validation

--- src/components/AddChargerDialog.js
+++ src/components/AddChargerDialog.js
@@ -85,6 +85,225 @@ export default function AddChargerDialog(props) {
   // const handlePostData = async () => {
   // };
 
+  const handleExitTitle = e => {
+    const { value } = e.target;
+
+  if (value === "") {
+      setForm({ ...form, ["errorTitle"]: true, ["helperTextTitle"]: "Title is empty" })
+    } else {
+      setForm({ ...form, ["errorTitle"]: false, ["helperTextTitle"]: "" })
+    }
+  }
+
+  const handleExitCostPerKwh = e => {
+    const { value } = e.target;
+    const cpk = validateCostPerKwh(value);
+
+  function validateCostPerKwh(costPerKWh) {
+    var characteres = /^(\(?\+?[0-9]*\)?)?[0-9_\-()]*$/;
+    return characteres.test(String(costPerKWh));
+  }
+
+   if (!cpk && value !== "") {
+      setForm({ ...form, ["errorCostPerKwh"]: true, ["helperTextCostPerKwh"]: "Characters are not allowed on cost per KWh" })
+    } else if (value === "") {
+      setForm({ ...form, ["errorCostPerKwh"]: true, ["helperTextCostPerKwh"]: "Cost per KWh is empty" })
+    } else {
+      setForm({ ...form, ["errorCostPerKwh"]: false, ["helperTextCostPerKwh"]: "" })
+    }
+  }
+
+  const handleExitNumberOfChargers = e => {
+    const { value } = e.target;
+    const noc = validateNumberOfChargers(value);
+
+  function validateNumberOfChargers(numberOfChargers) {
+    var characteres = /^(\(?\+?[0-9]*\)?)?[0-9_\-()]*$/;
+    return characteres.test(String(numberOfChargers));
+  }
+
+   if (!noc && value !== "") {
+      setForm({ ...form, ["errorNumberOfChargers"]: true, ["helperTextNumberOfChargers"]: "Characters are not allowed on number of chargers" })
+    } else if (value === "") {
+      setForm({ ...form, ["errorNumberOfChargers"]: true, ["helperTextNumberOfChargers"]: "Number of Chargers is empty" })
+    } else {
+      setForm({ ...form, ["errorNumberOfChargers"]: false, ["helperTextNumberOfChargers"]: "" })
+    }
+  }
+
+  const handleExitStreet = e => {
+    const { value } = e.target;
+    const db = validateStreet(value);
+
+  function validateStreet(street) {
+    var characteres = /^[a-zA-Z0-9,.!? ]*$/i;
+    return characteres.test(String(street));
+  }
+
+   if (!db && value !== "") {
+      setForm({ ...form, ["errorStreet"]: true, ["helperTextStreet"]: "Special characteres (!@#$%ˆ&*) are not allowed on street" })
+    } else if (value === "") {
+      setForm({ ...form, ["errorStreet"]: true, ["helperTextStreet"]: "Street is empty" })
+    } else {
+      setForm({ ...form, ["errorStreet"]: false, ["helperTextStreet"]: "" })
+    }
+  }
+
+  const handleExitCity = e => {
+    const { value } = e.target;
+    const ct = validateCity(value);
+
+  function validateCity(city) {
+    var characteres = /^[a-zA-Z0-9,.]{1,}$/;
+    return characteres.test(String(city));
+  }
+
+   if (!ct && value !== "") {
+      setForm({ ...form, ["errorCity"]: true, ["helperTextCity"]: "Special characteres (!@#$%ˆ&*) are not allowed on city" })
+    } else if (value === "") {
+      setForm({ ...form, ["errorCity"]: true, ["helperTextCity"]: "City is empty" })
+    } else {
+      setForm({ ...form, ["errorCity"]: false, ["helperTextCity"]: "" })
+    }
+  }
+
+  const handleExitStateOrProvince = e => {
+    const { value } = e.target;
+    const ct = validateStateOrProvince(value);
+
+  function validateStateOrProvince(stateOrProvince) {
+    var characteres = /[A-Z].*[A-Z]/;
+    return characteres.test(String(stateOrProvince));
+  }
+
+   if (!ct && value !== "") {
+      setForm({ ...form, ["errorStateOrProvince"]: true, ["helperTextStateOrProvince"]: "Please, use max. 2 UPPER CASE characters (e.g: BC). Special characteres (!@#$%ˆ&*) or numbers are not allowed on state of province" })
+    } else if (value === "") {
+      setForm({ ...form, ["errorStateOrProvince"]: true, ["helperTextStateOrProvince"]: "State or Province is empty" })
+    } else {
+      setForm({ ...form, ["errorStateOrProvince"]: false, ["helperTextStateOrProvince"]: "" })
+    }
+  }
+
+  const handleExitPostalCode = e => {
+    const { value } = e.target;
+    const pc = validatePostalCode(value);
+
+    function validatePostalCode(postalCode) {
+      var characteres = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+      return characteres.test(String(postalCode));
+    }
+
+    if (!pc && value !== "") {
+      setForm({ ...form, ["errorPostalCode"]: true, ["helperTextPostalCode"]: "Postal code cannot contain special characteres (-,@#!$%ˆ&*) or blank spaces" })
+    } else if (value === "") {
+      setForm({ ...form, ["errorPostalCode"]: true, ["helperTextPostalCode"]: "Postal code is empty" })
+    } else {
+      setForm({ ...form, ["errorPostalCode"]: false, ["helperTextPostalCode"]: "" })
+    }
+  }
+
+  const handleExitCountry = e => {
+    const { value } = e.target;
+    const ct = validateCountry(value);
+
+  function validateCountry(country) {
+    var characteres = /^[A-Z]+$/;
+    return characteres.test(String(country));
+  }
+
+    if (!ct && value !== "") {
+      setForm({ ...form, ["errorCountry"]: true, ["helperTextCountry"]: "Invalid Country" })
+    } else if (value === "") {
+      setForm({ ...form, ["errorCountry"]: true, ["helperTextCountry"]: "Country is empty" })
+    } else {
+      setForm({ ...form, ["errorCountry"]: false, ["helperTextCountry"]: "" })
+    }
+  }
+
+  const handleExitGeneralComments = e => {
+    const { value } = e.target;
+
+  if (value === "") {
+      setForm({ ...form, ["errorGeneralComments"]: true, ["helperTextGeneralComments"]: "General Comments is empty" })
+    } else {
+      setForm({ ...form, ["errorGeneralComments"]: false, ["helperTextGeneralComments"]: "" })
+    }
+  }
+
+  // const handleExitDateAvailableStart = e => {
+  //   const { value } = e.target;
+
+  //   let dateAvStart = new Date(value);
+
+  //   console.log((dateAvStart));
+  //   console.log(new Date(value))
+    
+  //   if (value !== "" && dateAvStart < new Date()) {
+  //     setForm({ ...form, ["errorDateAvailableStart"]: true, ["helperTextDateAvailableStart"]: "Date available start is invalid" })
+  //     } else if (dateAvStart === "") {
+  //       setForm({ ...form, ["errorDateAvailableStart"]: true, ["helperTextDateAvailableStart"]: "Date available start is empty" })
+  //     } else {
+  //       setForm({ ...form, ["errorDateAvailableStart"]: false, ["helperTextDateAvailableStart"]: "" })
+  //     }
+  //   }
+
+  // const handleExitDateAvailableEnd = e => {
+  //   const { value } = e.target;
+
+  //   let dateAvEnd = new Date(value);
+  //   let dateAvStart = new Date(form.dateAvailableStart)
+
+  //   console.log((dateAvEnd));
+    
+    
+    
+  //   if (value !== "" && dateAvEnd < dateAvStart) {
+  //     setForm({ ...form, ["errorDateAvailableEnd"]: true, ["helperTextDateAvailableEnd"]: "Date available end must be after start date" })
+  //     } else if (dateAvEnd === "") {
+  //       setForm({ ...form, ["errorDateAvailableEnd"]: true, ["helperTextDateAvailableEnd"]: "Date available end is empty" })
+  //     } else {
+  //       setForm({ ...form, ["errorDateAvailableEnd"]: false, ["helperTextDateAvailableEnd"]: "" })
+  //     }
+  //   } 
+
+  // const handleExitHourStart = e => {
+  //   // const { value } = e.target;
+
+  //   // let dateAvStart = new Date(value);
+
+  //   // console.log((dateAvStart));
+  //   // console.log(new Date(value))
+    
+  //   // if (value !== "" && dateAvStart < new Date()) {
+  //   //   setForm({ ...form, ["errorDateAvailableStart"]: true, ["helperTextDateAvailableStart"]: "Date available start is invalid" })
+  //   //   } else if (dateAvStart === "") {
+  //   //     setForm({ ...form, ["errorDateAvailableStart"]: true, ["helperTextDateAvailableStart"]: "Date available start is empty" })
+  //   //   } else {
+  //   //     setForm({ ...form, ["errorDateAvailableStart"]: false, ["helperTextDateAvailableStart"]: "" })
+  //   //   }
+  //   }
+
+  // const handleExitHourEnd = e => {
+  //   // const { value } = e.target;
+
+  //   // let dateAvEnd = new Date(value);
+  //   // let dateAvStart = new Date(form.dateAvailableStart)
+
+  //   // console.log((dateAvEnd));
+    
+    
+    
+  //   // if (value !== "" && dateAvEnd < dateAvStart) {
+  //   //   setForm({ ...form, ["errorDateAvailableEnd"]: true, ["helperTextDateAvailableEnd"]: "Date available end must be after start date" })
+  //   //   } else if (dateAvEnd === "") {
+  //   //     setForm({ ...form, ["errorDateAvailableEnd"]: true, ["helperTextDateAvailableEnd"]: "Date available end is empty" })
+  //   //   } else {
+  //   //     setForm({ ...form, ["errorDateAvailableEnd"]: false, ["helperTextDateAvailableEnd"]: "" })
+  //   //   }
+  //   } 
+
+
   return (
     <div>
       <Dialog
@@ -105,8 +324,11 @@ export default function AddChargerDialog(props) {
               label="Title"
               type="text"
               fullWidth
+              error={form.errorTitle}
+              helperText={form.helperTextTitle}
               value={form.title}
               onChange={handleInputChange}
+              onBlur={handleExitTitle}
             />
             <TextField
               margin="dense"
@@ -114,8 +336,11 @@ export default function AddChargerDialog(props) {
               label="Cost per KWh"
               type="number"
               fullWidth
+              error={form.errorCostPerKwh}
+              helperText={form.helperTextCostPerKwh}
               value={form.costPerKWh}
               onChange={handleInputChange}
+              onBlur={handleExitCostPerKwh}
             />
             <TextField
               margin="dense"
@@ -123,8 +348,11 @@ export default function AddChargerDialog(props) {
               label="Number of chargers"
               type="number"
               fullWidth
+              error={form.errorNumberOfChargers}
+              helperText={form.helperTextNumberOfChargers}
               value={form.numberOfChargers}
               onChange={handleInputChange}
+              onBlur={handleExitNumberOfChargers}
             />
             <TextField
               margin="dense"
@@ -132,8 +360,11 @@ export default function AddChargerDialog(props) {
               label="Street"
               type="text"
               fullWidth
+              error={form.errorStreet}
+              helperText={form.helperTextStreet}
               value={form.street}
               onChange={handleInputChange}
+              onBlur={handleExitStreet}
             />
             <TextField
               margin="dense"
@@ -141,8 +372,11 @@ export default function AddChargerDialog(props) {
               label="City"
               type="text"
               fullWidth
+              error={form.errorCity}
+              helperText={form.helperTextCity}
               value={form.city}
               onChange={handleInputChange}
+              onBlur={handleExitCity}
             />
             <TextField
               margin="dense"
@@ -150,8 +384,11 @@ export default function AddChargerDialog(props) {
               label="State Or Province"
               type="text"
               fullWidth
+              error={form.errorStateOrProvince}
+              helperText={form.helperTextStateOrProvince}
               value={form.stateOrProvince}
               onChange={handleInputChange}
+              onBlur={handleExitStateOrProvince}
             />
             <TextField
               margin="dense"
@@ -159,8 +396,12 @@ export default function AddChargerDialog(props) {
               label="Postal Code"
               type="text"
               fullWidth
+              error={form.errorPostalCode}
+              helperText={form.helperTextPostalCode}
               value={form.postCode}
               onChange={handleInputChange}
+              onBlur={handleExitPostalCode}
+
             />
             <TextField
               margin="dense"
@@ -168,8 +409,11 @@ export default function AddChargerDialog(props) {
               label="Country"
               type="text"
               fullWidth
+              error={form.errorCountry}
+              helperText={form.helperTextCountry}
               value={form.countryId}
               onChange={handleInputChange}
+              onBlur={handleExitCountry}
             />
             <TextField
               margin="dense"
@@ -177,8 +421,12 @@ export default function AddChargerDialog(props) {
               label="General Comments"
               type="text"
               fullWidth
+              // error={form.errorGeneralComments}
+              // helperText={form.helperTextGeneralComments}
               value={form.generalComments}
               onChange={handleInputChange}
+              // onBlur={handleExitGeneralComments}
+
             />
             <TextField
               margin="dense"
@@ -186,8 +434,12 @@ export default function AddChargerDialog(props) {
               label="Date Available Start"
               type="date"
               fullWidth
+              // error={form.errorDateAvailableStart}
+              // helperText={form.helperTextDateAvailableStart}
               value={form.dateAvailableStart}
               onChange={handleInputChange}
+              // onBlur={handleExitDateAvailableStart}
+
             />
             <TextField
               margin="dense"
@@ -195,8 +447,11 @@ export default function AddChargerDialog(props) {
               label="Date Available End"
               type="date"
               fullWidth
+              // error={form.errorDateAvailableEnd}
+              // helperText={form.helperTextDateAvailableEnd}
               value={form.dateAvailableEnd}
               onChange={handleInputChange}
+              // onBlur={handleExitDateAvailableEnd}
             />
             <TextField
               margin="dense"
@@ -204,8 +459,11 @@ export default function AddChargerDialog(props) {
               label="HourStart"
               type="text"
               fullWidth
+              // error={form.errorDateAvailableEnd}
+              // helperText={form.helperTextDateAvailableEnd}
               value={form.hourStart}
               onChange={handleInputChange}
+              // onBlur={handleExitDateAvailableEnd}
             />
             <TextField
               margin="dense"
@@ -213,8 +471,11 @@ export default function AddChargerDialog(props) {
               label="HourEnd"
               type="text"
               fullWidth
+              // error={form.errorDateAvailableEnd}
+              // helperText={form.helperTextDateAvailableEnd}
               value={form.hourEnd}
               onChange={handleInputChange}
+              // onBlur={handleExitDateAvailableEnd}
             />
             <TextField
               margin="dense"
@@ -222,8 +483,11 @@ export default function AddChargerDialog(props) {
               label="Connection Type Id"
               type="text"
               fullWidth
+              // error={form.errorDateAvailableEnd}
+              // helperText={form.helperTextDateAvailableEnd}
               value={form.connectionTypeId}
               onChange={handleInputChange}
+              // onBlur={handleExitDateAvailableEnd}
             />
           </DialogContent>
           <DialogActions>
